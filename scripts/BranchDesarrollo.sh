2290a8163919bfafa3804aa3cc1d63c95413d732
Ligdenia <ligdeyar.zep98@gmail.com>
2024-12-13 18:59:12 -0600

    2

---
+++ scripts/BranchDesarrollo.sh
@@ -0,0 +1,9 @@
+#Ejecucion de Trabajo  y repositorio local
+#cambios locales se integran a la rama desarrollo del repositorio remoto.
+echo "Sincronizando los cambios en la rama de Desarrollo..."
+git checkout Desarrollo
+git pull origin desarrollo
+
+
+#Completacion
+ echo "Tareas de desarrollo completadas."
