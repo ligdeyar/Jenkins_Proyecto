9249b8f6354d83ae715daac5825ac003456b8dc8
Ligdenia <ligdeyar.zep98@gmail.com>
2024-12-14 04:28:51 -0600

    p

--- scripts/GitClone-Pull.sh
+++ scripts/GitClone-Pull.sh
@@ -3,17 +3,13 @@ REPO_DIR="/var/lib/jenkins/workspace/Clone_Repository/Jenkins_Proyecto"
 REPO_URL="https://$GITHUB_USERNAME:$GITHUB_PASSWORD@github.com/ligdeyar/Jenkins_Proyecto.git"
 BRANCH="main"
 
+
 # Verificar si el directorio de destino ya existe
 if [ -d "$REPO_DIR" ]; then
-    # Si es un directorio y no está vacío, eliminamos el directorio
-    if [ "$(ls -A $REPO_DIR)" ]; then
-        echo "El directorio '$REPO_DIR' no está vacío, eliminando y clonando de nuevo..."
-        rm -rf $REPO_DIR  # Eliminar el directorio y su contenido
-    fi
-    # Volver a clonar el repositorio
-    git clone $REPO_URL $REPO_DIR
+    echo "El directorio '$REPO_DIR' ya existe, actualizando el repositorio..."
     cd $REPO_DIR
     git checkout $BRANCH
+    git pull origin $BRANCH  # Actualizar la rama local con los cambios del repositorio remoto
 else
     # Clonar el repositorio si no existe
     echo "Clonando el repositorio..."
