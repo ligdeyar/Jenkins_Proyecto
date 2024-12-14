0e69fe4114d16ce1685958288efa582a91c1010d
Ligdenia <ligdeyar.zep98@gmail.com>
2024-12-13 20:41:07 -0600

    3

---
+++ scripts/BranchProduccion.sh
@@ -0,0 +1,11 @@
+REPO_DIR= "/var/lib/jenkins/workspace/Clone_Repository/Jenkins_Proyecto"
+BRANCH="Desarrollo"
+#Ejecucion de Trabajo  y repositorio local
+#cambios locales se integran a la rama desarrollo del repositorio remoto.
+echo "Sincronizando los cambios en la rama de Desarrollo..."
+git checkout Desarrollo
+git pull origin desarrollo
+
+
+#Completacion
+ echo "Sincronizacion de desarrollo completadas."
