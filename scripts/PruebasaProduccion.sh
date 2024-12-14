c3d201930dbc13e739cceb1327a7669417ce7679
Ligdenia <ligdeyar.zep98@gmail.com>
2024-12-14 02:28:46 -0600

    Pruebas

---
+++ scripts/PruebasaProduccion.sh
@@ -0,0 +1,12 @@
+# Variables
+REPO_DIR="/var/lib/jenkins/workspace/Clone_Repository/Jenkins_Proyecto"
+REPO_URL="https://$GITHUB_USERNAME:$GITHUB_PASSWORD@github.com/ligdeyar/Jenkins_Proyecto.git"
+BRANCH="main"
+TESTING_BRANCH = 'Pruebas'
+
+
+git checkout $BRANCH
+git merge $TESTING_BRANCH
+git push origin $BRANCH
+
+echo "CAMBIOS SINCRONIADOS CORRECTAMENTE A PRODUCCION"
