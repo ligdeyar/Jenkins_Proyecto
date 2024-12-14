e023f35a71c8cb9e1124b74d75f186b98b09f2e5
Ligdenia <ligdeyar.zep98@gmail.com>
2024-12-13 23:23:52 -0600

    4.0

--- scripts/BranchDesarrollo.sh
+++ scripts/BranchDesarrollo.sh
@@ -1,5 +1,5 @@
 REPO_DIR= "/var/lib/jenkins/workspace/Clone_Repository/Jenkins_Proyecto"
-BRANCH="Desarrollo"
+BRANCH='Desarrollo'
 #Ejecucion de Trabajo  y repositorio local
 #cambios locales se integran a la rama desarrollo del repositorio remoto.
 echo "Sincronizando los cambios en la rama de Desarrollo..."
--- scripts/BranchProduccion.sh
+++ scripts/BranchProduccion.sh
@@ -1,11 +1,12 @@
 REPO_DIR= "/var/lib/jenkins/workspace/Clone_Repository/Jenkins_Proyecto"
-BRANCH="Desarrollo"
+BRANCH='Produccion'
 #Ejecucion de Trabajo  y repositorio local
 #cambios locales se integran a la rama desarrollo del repositorio remoto.
 echo "Sincronizando los cambios en la rama de Desarrollo..."
-git checkout Desarrollo
-git pull origin desarrollo
+cd $REPO_DIR
+git checkout $BRANCH
+git pull origin $BRANCH
 
 
 #Completacion
- echo "Sincronizacion de desarrollo completadas."
+ echo "Sincronizacion de Produccion completadas."
