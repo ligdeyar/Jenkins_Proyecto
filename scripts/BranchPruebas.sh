106bfef6def91bae7b1381b72be83362b8453d1f
Ligdenia <ligdeyar.zep98@gmail.com>
2024-12-14 01:10:05 -0600

    Pruebas

---
+++ scripts/BranchPruebas.sh
@@ -0,0 +1,28 @@
+REPO_DIR= "/var/lib/jenkins/workspace/Clone_Repository/Jenkins_Proyecto"
+BRANCH="Pruebas"
+
+#Ejecucion de Trabajo  y repositorio local
+#cambios aprobados en desarrollo se integran a la rama Pruebas del repositorio remoto.
+echo "..***************CAMBIOS APROBADOS****************.."
+echo "Sincronizando los cambios en la rama de Pruebas..."
+cd $REPO_DIR
+git checkout $BRANCH
+git pull origin $BRANCH
+
+#Fusionar los cambios de desarrollo a prueba
+echo "Fusionando cambios de Desarrollo a Pruebas..."
+git merge Desarrollo
+
+echo "Subiendo cambios fusionados a la rama Pruebas..."
+git push origin $BRANCH
+
+#Completacion de sincronizacion
+echo "Sincronizacion y Fusion de Desarrollo a Pruebas completadas."
+
+#Desplegadas y Aprobadas en el servidor de pruebas.
+echo "Desplegando los cambios en el servidor de pruebas..."
+
+echo "Simulando despliegue en servidor de pruebas..."
+
+#Completación final
+echo "Despliegue en pruebas completado."
--- scripts/GitClone-Pull.sh
+++ scripts/GitClone-Pull.sh
@@ -1,15 +1,23 @@
-#Variables
-REPO_DIR= "/var/lib/jenkins/workspace/Clone_Repository/Jenkins_Proyecto"
-REPO_URL="https://$GITHUB_USERNAME:$GITHUB_PASSWORD@github.com/ligdeyar/Prueba_Jenkins.git"
-BRANCH="main" 
+# Variables
+REPO_DIR="/var/lib/jenkins/workspace/Clone_Repository/Jenkins_Proyecto"
+REPO_URL="https://$GITHUB_USERNAME:$GITHUB_PASSWORD@github.com/ligdeyar/Jenkins_Proyecto.git"
+BRANCH="main"
 
-
-#Veriificar si ya existe el repositorio 
-if [ ! -d "$REPO_DIR" ]; then
-    echo "Clonando el repositorio..."
-    git clone $REPO_URL
-else
-    echo "El repositorio ya existe, actualizando..."
+# Verificar si el directorio de destino ya existe
+if [ -d "$REPO_DIR" ]; then
+    # Si es un directorio y no está vacío, eliminamos el directorio
+    if [ "$(ls -A $REPO_DIR)" ]; then
+        echo "El directorio '$REPO_DIR' no está vacío, eliminando y clonando de nuevo..."
+        rm -rf $REPO_DIR  # Eliminar el directorio y su contenido
+    fi
+    # Volver a clonar el repositorio
+    git clone $REPO_URL $REPO_DIR
     cd $REPO_DIR
-    git pull origin $BRANCH
+    git checkout $BRANCH
+else
+    # Clonar el repositorio si no existe
+    echo "Clonando el repositorio..."
+    git clone $REPO_URL $REPO_DIR
+    cd $REPO_DIR
+    git checkout $BRANCH
 fi
